273b174c9c9eb548fc8c5e8ed41303dcdd54c14f
supaheckafresh <neilstrain@gmail.com>
2016-03-15 10:17:34 -0700

    Put add contact form directly on the page (remove modal)

--- resources/views/index.php
+++ resources/views/index.php
@@ -11,45 +11,47 @@
 
 <body>
 <div class="container">
-    <div class="row">
+
         <h1>Contacts</h1>
         <hr>
             <div data-ng-app="contactsApp" data-ng-controller="ContactsController as contacts">
-                    <div class="row">
-                        <h4>Create a contact</h4>
-                        <div class="col-xs-12 form-custom">
-                            <form action="" class="form-inline">
-                                <div class="form-group">
-                                    <h3>Contacts Name:</h3>
-                                    <input name="first_name"
-                                           type="text"
-                                           placeholder="First Name"
-                                           class="form-control"> <br>
-                                    <input name="last_name"
-                                           type="text"
-                                           placeholder="Last Name"
-                                           class="form-control">
-                                    <h3>Email:</h3>
-                                    <input name="email"
-                                           type="text"
-                                           placeholder="Email"
-                                           class="form-control"><br/>
-                                    <h3>Phone:</h3>
-                                    <input name="phone"
-                                           type="text"
-                                           placeholder="Phone"
-                                           class="form-control"><br/>
-                                    <h3>Relation:</h3>
-                                    <select multiple class="form-control">
-                                        <option>Friend</option>
-                                        <option>Family</option>
-                                        <option>Co-Worker</option>
 
-                                    </select>
-                                </div>
-                            </form>
+                    <div class="row">
+                        <div class="col-xs-12">
+
+                            <h4>Create a contact</h4>
+                                <form action="" class="form-inline">
+
+                                        <input name="first_name"
+                                               type="text"
+                                               placeholder="First Name"
+                                               class="form-control">
+
+                                        <input name="last_name"
+                                               type="text"
+                                               placeholder="Last Name"
+                                               class="form-control">
+
+                                        <input name="email"
+                                               type="text"
+                                               placeholder="Email"
+                                               class="form-control">
+
+                                        <input name="phone"
+                                               type="text"
+                                               placeholder="Phone"
+                                               class="form-control">
+
+                                        <select multiple class="form-control">
+                                            <option>Friend</option>
+                                            <option>Family</option>
+                                            <option>Co-Worker</option>
+
+                                        </select>
+                                </form>
                         </div>
                     </div>
+
 
 
                     <table class="table">
@@ -77,8 +79,7 @@
                         </thead>
                 </div>
             </div>
-    </div>
-</div>
+
 
 
         <script src="/build/js/deps.js"></script>
