8e761a4697f213b0ebf1164a1b63c46fb403ac5b
Jordomav <jordomav@gmail.com>
2016-03-21 13:24:28 -0700

    order controller added

--- resources/js/filters/order.controller.js
+++ resources/js/filters/order.controller.js
@@ -7,6 +7,5 @@
     .controller('mainController', function($scope) {
         $scope.sortType     = 'name'; // set the default sort type
         $scope.sortReverse  = false;  // set the default sort order
-        $scope.searchFish   = '';     // set the default search/filter term
     });
 }());
