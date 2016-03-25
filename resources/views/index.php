54be2fd50a73b94d1856f219c8ce35191883e8af
supaheckafresh <neilstrain@gmail.com>
2016-03-24 17:54:05 -0700

    Add contact name to contact lists modal

--- resources/views/index.php
+++ resources/views/index.php
@@ -276,14 +276,14 @@
             </div>
         </div>
 
-        <!--    CONTACT LIST MODAL-->
+        <!-- Contact List Modal -->
         <div id="list-modal" class="modal fade" role="dialog">
             <div class="modal-dialog">
                 <!-- Modal content-->
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Contacts Lists</h4>
+                        <h4 class="modal-title">Contacts Lists for {{contacts.selectedContact.first_name + ' ' + contacts.selectedContact.last_name }}</h4>
                     </div>
                     <div class="modal-body">
                         <table class="table-condensed col-xs-6">
